d1121ec8c8aa3b8a70a2a9478212ef4549cee436
Tom <tmmydantucker@gmail.com>
2020-05-24 02:25:05 +0000

    predicted life satisfaction

---
+++ Scripts/hello.py
@@ -0,0 +1,9 @@
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Later World")
+
+# Main program starts here
+hello()
